203f21d117ebac76947445a16faffb6273b527ee
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-01-12 18:48:09 +0800

    initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import * as Device from "expo-device";
+import { Stack } from "expo-router";
+import * as ScreenOrientation from "expo-screen-orientation";
+
+if (Device.deviceType !== Device.DeviceType.TABLET) {
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+}
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
