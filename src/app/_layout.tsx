cfa6c2802637c277123296ff39dfda0b433256b2
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-01-12 19:10:13 +0800

    feat: adds rn-reusables

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,27 @@
 import "@/global.css";
+import { NAV_THEME } from "@/lib/theme";
+import { ThemeProvider } from "@react-navigation/native";
 
+import { PortalHost } from "@rn-primitives/portal";
 import * as Device from "expo-device";
 import { Stack } from "expo-router";
 import * as ScreenOrientation from "expo-screen-orientation";
+import { StatusBar } from "expo-status-bar";
+import { colorScheme } from "nativewind";
 
 if (Device.deviceType !== Device.DeviceType.TABLET) {
   ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
 }
 
 export default function RootLayout() {
+  const scheme = colorScheme.get() || "dark";
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ThemeProvider value={NAV_THEME[scheme]}>
+      <StatusBar style={scheme} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+      <PortalHost />
+    </ThemeProvider>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="font-bold text-2xl">
+      <Text className="text-foreground font-bold text-2xl">
         Edit app/index.tsx to edit this screen.
       </Text>
     </View>
