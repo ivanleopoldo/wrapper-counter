e3a9ac8c4090a70094cd5baaa706066ea6215385
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-01-20 17:23:42 +0800

    WIP: adds temporary counter

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,40 +1,36 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { Text } from "@/components/ui/text";
+import React from "react";
 import { View } from "react-native";
 
 export default function Index() {
+  const [sold, setSold] = React.useState(0);
+
   return (
     <View className="gap-4 md:flex-row">
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Text>Card Content</Text>
-        </CardContent>
-        <CardFooter>
-          <Text>Card Footer</Text>
-        </CardFooter>
+      <Card className="p-4">
+        <View>
+          <Text className="text-4xl font-extrabold">Sold</Text>
+          <Text className="text-4xl font-bold">{sold}</Text>
+        </View>
+        <View>
+          <Button onPress={() => setSold(sold + 1)} className="mt-4 mb-2">
+            <Text>Add</Text>
+          </Button>
+          <Button
+            onPress={() => {
+              if (sold > 0) setSold(sold - 1);
+            }}
+          >
+            <Text>Subtract</Text>
+          </Button>
+        </View>
       </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Text>Card Content</Text>
-        </CardContent>
-        <CardFooter>
-          <Text>Card Footer</Text>
-        </CardFooter>
+      <Card className="p-4">
+        <View>
+          <Text>hello</Text>
+        </View>
       </Card>
     </View>
   );
